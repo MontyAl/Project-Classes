ce3e5d9ac2789650636ee9e4b07739d89f1e0544
nabilmekhalfa <131925631+nabilmekhalfa@users.noreply.github.com>
2023-04-26 22:25:23 -0700

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -97,47 +97,7 @@ class DoctorManager:
         doctor_id = input("Enter the doctor ID: ")
         for doctor in self.doctors:
 
-            class Patient:
-                def __init__(self, id, name, disease, gender, age):
-                    self.id = id
-                    self.name = name
-                    self.disease = disease
-                    self.gender = gender
-                    self.age = age
-
-                def get_id(self):
-                    return self.id
-
-                def get_name(self):
-                    return self.name
-
-                def get_disease(self):
-                    return self.disease
-
-                def get_gender(self):
-                    return self.gender
-
-                def get_age(self):
-                    return self.age
-
-                def set_id(self, id):
-                    self.id = id
-
-                def set_name(self, name):
-                    self.name = name
-
-                def set_disease(self, disease):
-                    self.disease = disease
-
-                def set_gender(self, gender):
-                    self.gender = gender
-
-                def set_age(self, age):
-                    self.age = age
-
-                def __str__(self):
-                    return f"{self.id}_{self.name}_{self.disease}_{self.gender}_{self.age}"
-
+            
 class Patient:
     def __init__(self, id, name, disease, gender, age):
         self.id = id
